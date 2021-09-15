43ef3da4fa04d1699874dfcbab6187e0999b81c7
danascape <danascape@gmail.com>
2021-09-15 12:17:55 +0200

    ginkgo-sepolicy: Regenerate * Moved perfmgr policy to pixel-sepolicy

--- ginkgo-sepolicy.mk
+++ ginkgo-sepolicy.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020-21 Raphielscape LLC. and Haruka LLC.
+# Copyright (C) 2020-21 Raphielscape LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,22 +14,11 @@
 # limitations under the License.
 #
 
+# Qcom vendors
+BOARD_SEPOLICY_DIRS += device/xiaomi/ginkgo-sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/xiaomi/ginkgo-sepolicy/vendor/qcom/sm6125
+
 # Xiaomi
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/ginkgo-sepolicy/private
 
-BOARD_SEPOLICY_DIRS += device/xiaomi/ginkgo-sepolicy/vendor/qcom/sm6125
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/adsprpcd \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/audio \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/bluetooth \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/camera \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/cdsprpcd \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/fingerprint \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/ir \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/kcal \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/light \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/misc \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/neuralnetworks \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/power \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/radio \
-    device/xiaomi/ginkgo-sepolicy/vendor/xiaomi/thermal
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/ginkgo-sepolicy/vendor/xiaomi
